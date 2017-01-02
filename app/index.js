8b24f316439ebb58dab54aa04f09e55a33d78cbb
Paulius <andriekus.paulius@gmail.com>
2017-01-03 00:07:05 +0200

    bug fix and refactoring

--- app/index.js
+++ app/index.js
@@ -1,5 +1,19 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
+var PropTypes = React.PropTypes;
+
+const PepeSingleton = {
+  angry: "http://i3.kym-cdn.com/entries/icons/original/000/017/318/angry_pepe.jpg",
+  happy: "http://i1.kym-cdn.com/photos/images/facebook/000/862/065/0e9.jpg",
+  sneaky: "http://i2.kym-cdn.com/photos/images/newsfeed/000/977/021/a14.jpg",
+  good: "http://i1.kym-cdn.com/photos/images/facebook/000/095/218/feels-good-man.jpg",
+  crying: "http://images.8tracks.com/cover/i/008/769/353/tumblr_ndo4s3xQln1qlpyreo8_1280-9891.jpg?rect=3,0,633,633&q=98&fm=jpg&fit=max&w=320&h=320",
+  resting: "https://pbs.twimg.com/media/CayijGDUYAAJcAk.jpg"
+};
+
+var getSelectedIndex = function(e) {
+  return parseInt(e.target.parentElement.attributes.getNamedItem('data-index').value);
+};
 
 var Noob = React.createClass({
 
@@ -9,18 +23,19 @@ var Noob = React.createClass({
     return {
       value: "",
       pepe: "",
-      todoList: ["wipe", "butt"]
+      todoList: [
+        {title: "ayy", complete: true},
+        {title: "lmao", complete: false}
+      ]
     }
 
   },
 
   handleChange: function(e) {
 
-    // this.inputValue = e.target.value;
-    console.log("kon ci spaudi?",this.state.value);
     this.setState({
       value: e.target.value,
-      pepe: "http://i1.kym-cdn.com/photos/images/facebook/000/862/065/0e9.jpg"
+      pepe: PepeSingleton.happy
     });
 
   },
@@ -28,94 +43,68 @@ var Noob = React.createClass({
   handleSubmit: function(e) {
     e.preventDefault();
 
-    console.log("bebi!",this.state.value);
     if (this.state.value === "") {
-      this.setState({pepe: "http://i3.kym-cdn.com/entries/icons/original/000/017/318/angry_pepe.jpg"})
+      this.setState({pepe: PepeSingleton.angry})
       return false;
     }
 
-    var list_array = this.state.todoList;
-    list_array.push(this.state.value);
+    var tasks_array = this.state.todoList;
+    var new_task = {title: this.state.value, complete: false};
+
+    tasks_array.push(new_task);
+
     this.setState({
       value: "",
-      pepe: "http://i2.kym-cdn.com/photos/images/newsfeed/000/977/021/a14.jpg",
-      todoList: list_array
+      pepe: PepeSingleton.sneaky,
+      todoList: tasks_array
     });
 
   },
 
   handleRemove: function(e) {
 
-    var index = e.target.attributes.getNamedItem('data-index').value;
-    var updated_list = this.state.todoList.filter(function (item, i) {
-       return index != i ? item : "";
+    var new_tasks_array = this.state.todoList.filter(function (task, i) {
+      return getSelectedIndex(e) != i ? task : "";
     });
 
     this.setState({
-      pepe: "http://i1.kym-cdn.com/photos/images/facebook/000/095/218/feels-good-man.jpg",
-      todoList: updated_list
+      pepe: PepeSingleton.good,
+      todoList: new_tasks_array
     });
 
   },
 
   handleDone: function(e) {
-    var el = e.target;
-    var el_parent = el.parentElement.classList;
 
-    if (!el_parent.contains('done')) {
-      el_parent.add("done");
-      el.innerHTML = "Undo";
-      this.setState({
-        pepe: "https://pbs.twimg.com/media/CayijGDUYAAJcAk.jpg",
-      });
+    var index = getSelectedIndex(e);
+    var new_tasks_array = this.state.todoList.map(function (task, i) {
 
-    } else {
-      el_parent.remove("done");
-      el.innerHTML = "Done";
-      this.setState({
-        pepe: "http://images.8tracks.com/cover/i/008/769/353/tumblr_ndo4s3xQln1qlpyreo8_1280-9891.jpg?rect=3,0,633,633&q=98&fm=jpg&fit=max&w=320&h=320",
-      });
-    }
+      if (index === i) {
+        !task.complete ? task.complete = true : task.complete = false;
+      }
+
+      return task;
+
+    });
+
+    var pepe_reaction = new_tasks_array[index].complete ? PepeSingleton.resting : PepeSingleton.crying;
+
+    this.setState({
+      pepe: pepe_reaction,
+      todoList: new_tasks_array
+    });
 
   },
 
   render: function() {
 
     return (
-      <div className="col-sm-6 col-sm-offset-3 text-center">
-        <h1>Heyo, Noob!</h1>
-        <form>
-          <input type="text" className="form-control" value={this.state.value} onChange={this.handleChange} />
-          <button type="button" className="btn btn-lg btn-primary" onClick={this.handleSubmit} style={{margin: 10 + 'px'}}>Add a task</button>
-        </form>
-        <div>
-          <h3>Todo List</h3>
-          <ul className="list-group">
-            {this.state.todoList.map(function(task, index) {
-              return (
-                <li className="list-group-item clearfix" key={index}>
-                  <span className="pull-left task">{task}</span>
-                  <span
-                    className="btn btn-sm btn-danger pull-right"
-                    data-index={index}
-                    onClick={this.handleRemove}>
-                      Remove
-                  </span>
-                  <span
-                    className="btn btn-sm btn-success pull-right"
-                    onClick={this.handleDone}
-                    style={{marginRight: 10 + "px"}}>
-                      Done
-                  </span>
-                </li>
-              )
-            }.bind(this))}
-          </ul>
-        </div>
-        <div style={{marginBottom: 30 + "px"}}>
-          <Pepe pepe={this.state.pepe} />
-        </div>
-      </div>
+      <MainContainer>
+        <Title />
+        <Form onChange={this.handleChange} onSubmit={this.handleSubmit} value={this.state.value} />
+        <TodoList todoList={this.state.todoList} onRemove={this.handleRemove} toggleDone={this.handleDone} />
+        <Pepe pepe={this.state.pepe} />
+      </MainContainer>
     )
 
   }
@@ -123,42 +112,101 @@ var Noob = React.createClass({
 
 });
 
-const Pepe = function(props) {
-  // console.log(props.pepe);
-  // return (
-    if (props.pepe != "") {
-      return (
-        <div>
-          <img src={props.pepe} alt="" style={{height: "auto",width: 100 + "%"}}/>
-        </div>
-      )
-    }
-    return (<div></div>)
-  // )
+
+const MainContainer = function(props) {
+
+  return (
+    <div className="col-sm-6 col-sm-offset-3 text-center">
+      {props.children}
+    </div>
+  )
+
+}
+
+
+const Title = function(props) {
+
+  return (
+    <h1>Heyo, Noob!</h1>
+  )
+
+}
+
+
+const Form = function(props) {
+
+  return (
+    <form>
+      <input
+        type="text"
+        className="form-control"
+        value={props.value}
+        onChange={props.onChange} />
+      <button
+        type="button"
+        className="btn btn-lg btn-primary"
+        onClick={props.onSubmit}
+        style={{margin: 10 + 'px'}}>
+          Add a task
+      </button>
+    </form>
+  )
+
 };
 
-// <List items={this.state.todoList}></List>
 
-// const List = function(props) {
-//   console.log(props.items);
-//   return (
-//     <div>
-//       <h3>Todo List</h3>
-//       <ul className="list-group">
-//         {props.items.map(function(item, index) {
-//           return (
-//             <li className="list-group-item clearfix" key={index}>
-//               <span className="pull-left">{item}</span>
-//               <span className="btn btn-sm btn-danger pull-right" onClick={}>Remove</span>
-//               <span className="btn btn-sm btn-success pull-right" style={{marginRight: 10 + "px"}}>Done</span>
-//             </li>
-//           )
-//         })}
-//       </ul>
-//     </div>
-//   )
+const TodoList = function(props) {
+
+  return (
+    <div>
+      <ul className="list-group">
+        {props.todoList.map(function(task, index) {
+          return (
+            <li className={"list-group-item clearfix" + (task.complete ? " done" : "")} data-index={index} key={index}>
+              <span className="pull-left task">{task.title}</span>
+              <span
+                className="btn btn-sm btn-danger pull-right"
+                onClick={props.onRemove}>
+                  Remove
+              </span>
+              <span
+                className="btn btn-sm btn-success pull-right"
+                onClick={props.toggleDone}
+                style={{marginRight: 10 + "px"}}>
+                  {task.complete ? "Undo" : "Done"}
+              </span>
+            </li>
+          )
+        }.bind(this))}
+      </ul>
+    </div>
+  )
+};
+
+
+const Pepe = function(props) {
+
+  if (props.pepe != "") {
+    return (
+      <div style={{marginBottom: 30 + "px"}}>
+        <img src={props.pepe} alt="" style={{height: "auto",width: 100 + "%"}}/>
+      </div>
+    )
+  }
+
+  return (<div></div>)
+
+};
+
+
+// Noob.propTypes = {
+//   value: PropTypes.string,
+//   pepe: PropTypes.string,
+//   todoList: PropTypes.arrayOf(PropTypes.shape({
+//      title: PropTypes.string.isRequired,
+//      complete: PropTypes.bool.isRequired,
+//    })).isRequired
 // }
-
 
 ReactDOM.render(
   <Noob />,
